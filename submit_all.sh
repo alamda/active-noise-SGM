f0eddc291e55e4e8dcf76b2d80c44d9442498f07
alamda <alexandral@uchicago.edu>
2024-05-26 16:56:21 -0500

    rewrote submit script to include cmd line args and sbatch.sh file generation over a series of params

--- submit_all.sh
+++ submit_all.sh
@@ -2,39 +2,58 @@
 
 port=7021
 
-dirdir=("simulations_ml"
-	    "simulations_rv1"
-		"simulations_rv2")
+root_dir="$(pwd)"
+sim_dir="simulations"
 
-sims=("vpsde_multiswiss"
-      "vpsde_multiswiss_overlap"
-	  "vpsde_swiss"
-	  "cld_multiswiss"
-	  "cld_multiswiss_overlap"
-	  "cld_swiss"
-	  "passive_multiswiss"
-	  "passive_multiswiss_overlap"
-	  "passive_swiss"
-	  "active_multiswiss"
-	  "active_multiswiss_overlap"
-	  "active_swiss")
+[ ! -d $sim_dir ] && mkdir $sim_dir
 
-for dir in "${dirdir[@]}"
+weightings=("reweightedv2"
+			#"likelihood"
+			#"reweightedv1"
+		   )
+
+sdes=("vpsde"
+	  "cld"
+	  "passive"
+	  "active"
+	 )
+
+datasets=(#"multimodal_swissroll"
+		  "multimodal_swissroll_overlap"
+		  #"swissroll"
+		  )
+
+taus=("0.01"
+	  #"0.05"
+	  "0.1"
+	  #"0.25"
+	  "0.5"
+	  #"0.75"
+	  )
+
+cd $sim_dir
+
+for weighting in "${weightings[@]}"
 do
+	for sde in "${sdes[@]}"
+	do
+		for dataset in "${datasets[@]}"
+		do
+			for tau in "${taus[@]}"
+			do
+				sim_sub_dir="${weighting}_${sde}_${dataset}_tau${tau}"
 
-for sim in "${sims[@]}"
-do
-		
-	root_dir="${dir}/${sim}"
+				[ ! -d $sim_sub_dir ] && mkdir $sim_sub_dir
+				
+				echo 'sbatch --account=pi-dfreedman --partition=schmidt-gpu --gres=gpu:1 --qos=schmidt --time=3:00:00 --name '${sim_sub_dir}' --wrap "echo '${sim_sub_dir}' ; conda run -n pytorch python '${root_dir}'/main.py -cc '${root_dir}'/config_toy.txt --root '${sim_sub_dir}' --workdir work_dir/dataset --master_port '${port}' --weighting '${weighting}' --sde '${sde}' --dataset '${dataset}' --tau '${tau}'"' > ${sim_sub_dir}/sbatch.sh
 
-	if [ ! -d $root_dir ]
-	then
-		sbatch --account=pi-dfreedman --partition=schmidt-gpu --gres=gpu:1 --qos=schmidt --time=3:00:00 --wrap "conda run -n pytorch python main.py -cc ${dir}/config_${sim}_train.txt --root $root_dir --workdir work_dir/dataset --master_port $port"
+				((port++))
+				
+				sh $sim_sub_dir/sbatch.sh
+			done
 
-		((port++))
-	fi
-
+		done
+	done
 done
 
-done
-
+cd $root_dir
