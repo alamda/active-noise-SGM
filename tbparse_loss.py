54f2aae6d0525445b67cc82ed922d337adebadef
alamda <alexandral@uchicago.edu>
2024-06-18 10:57:23 -0500

    changed ylims in loss plotting with tbparse

--- tbparse_loss.py
+++ tbparse_loss.py
@@ -14,7 +14,7 @@ loss_arr = loss_df.value.values
 fig = Figure()
 ax = fig.subplots()
 
-ax.set_ylim(bottom=0.001, top=1000)
+ax.set_ylim(bottom=0.00001, top=1)
 ax.set_yscale('log')
 ax.plot(step_arr, loss_arr)
 
