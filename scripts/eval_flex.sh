8c1231cc09696b8a5217565d2a1662404a5c9da8
alamda <alexandral@uchicago.edu>
2024-06-26 17:20:13 -0500

    added script for running multiple evals of trained models

---
+++ scripts/eval_flex.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+datasets=("diamond_close"
+          "multigaussian_2D_close"
+          #"swissroll"
+		  #"multimodal_swissroll"
+		  "multimodal_swissroll_overlap"
+		  "diamond"
+		  #"multigaussian_1D"
+		  "multigaussian_2D"
+		  )
+
+snapshot_freqs=(#"10"
+				"100"
+				"1000"
+				#"10000"
+				#"100000"
+				)
+
+out_file="progress.out"
+
+[ -e $out_file ] && rm $out_file
+
+dirdir=(#"simulations_ml"
+	    #"simulations_rv1"
+		"simulations_rv2"
+		)
+
+sims=("vpsde"
+	  "passive"
+	  "active"
+	  "cld"
+      )
+
+for dir in "${dirdir[@]}"
+do
+
+for snapshot_freq in "${snapshot_freqs[@]}"
+do
+
+for dataset in "${datasets[@]}"
+do
+
+	n_train_iters="${snapshot_freq}00"
+
+for sim in "${sims[@]}"
+do
+		
+	root_dir="${dir}/${sim}_${dataset}_${n_train_iters}"
+
+	# if [ ! -d $root_dir ]
+	# then
+        echo "starting $root_dir" >> $out_file
+		
+        python main.py -cc ${dir}/config_${sim}_eval.txt --root $root_dir --workdir work_dir/dataset --dataset $dataset --snapshot_freq $snapshot_freq --n_train_iters $n_train_iters --ckpt_file final_checkpoint.pth --eval_folder eval --eval_sample --eval_sample_samples 512
+		
+		echo "$root_dir done" >> $out_file
+	# else
+	# 	echo "$root_dir directory exists, not running" >> $out_file
+	# fi
+
+done
+
+done
+
+done
+
+done
