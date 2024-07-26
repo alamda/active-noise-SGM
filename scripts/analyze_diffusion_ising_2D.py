cedfa000fcf1f940ed778a2fa048d8ff8399393a
alamda <alexandral@uchicago.edu>
2024-07-26 16:35:15 -0500

    added script for analyzing the samples generated during diffusion training

---
+++ scripts/analyze_diffusion_ising_2D.py
@@ -0,0 +1,49 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))
+
+from util.ising_2D import Ising2D
+
+if __name__=='__main__':
+    all_subdirs = [d for d in os.listdir('.') if os.path.isdir(d)]
+    
+    iter_list = []
+    energy_list = []
+    mag_list = []
+    
+    for dirs in all_subdirs:
+        if 'iter_' in dirs:
+            dir_name = dirs
+            iter_num_str = dir_name.replace('iter_', '')
+            
+            iter_num = int(iter_num_str)
+            
+            iter_list.append(iter_num)
+            
+            file_path = os.path.join(dirs, "sample_x.npy")
+            
+            state = np.load(file_path)
+            
+            state = np.reshape(state, (state.shape[-2], state.shape[-1]))
+            
+            ising = Ising2D(state=state)
+            
+            energy_list.append(ising.energy/state.size)
+            mag_list.append(ising.mag/state.size)
+            
+    
+    fig, axs = plt.subplots(1, 2)
+    
+    axs[0].set_ylim(-1,1)
+    axs[1].set_ylim(0,1)
+
+    axs[0].scatter(iter_list, energy_list, clip_on=False)
+    axs[1].scatter(iter_list, np.absolute(np.array(mag_list)), clip_on=False)
+    
+    plt.show()
+            
+            
