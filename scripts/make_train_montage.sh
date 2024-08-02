98ebfc66e72fffcd96f076f2a9aab1e8a5875a3b
alamda <alexandral@uchicago.edu>
2024-08-02 16:34:55 -0500

    added script to make montage out of training data snapshots

---
+++ scripts/make_train_montage.sh
@@ -0,0 +1,6 @@
+find . -maxdepth 2 -name "train_*.png" | sort -V > pnglist.txt
+
+montage -density 900 -tile 10x0 -geometry +5+50 -border 10 @pnglist.txt all_train.png
+
+#&& ffmpeg -i sample.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" sample.mp4 && vlc sample.mp4
+
