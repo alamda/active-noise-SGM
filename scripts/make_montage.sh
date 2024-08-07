116292806fb2ee1eb2cc62d6ca4f07f12c5dbd06
alamda <alexandral@uchicago.edu>
2024-08-07 14:55:30 -0500

    added title to sample and train data grids and changed grid size

--- scripts/make_montage.sh
+++ scripts/make_montage.sh
@@ -1,6 +1,6 @@
 find . -maxdepth 2 -name "sample*.png" | sort -V > pnglist.txt
 
-montage -density 900 -tile 10x0 -geometry +5+50 -border 10 @pnglist.txt all_samples.png
+montage -density 900 -title "Diffusion Samples" -tile 5x0 -geometry +5+50 -border 10 @pnglist.txt all_samples.png
 
 #&& ffmpeg -i sample.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" sample.mp4 && vlc sample.mp4
 
